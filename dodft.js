1ac1cc5ac7f4805260e77da6ab2de4d7afab2a25
apurbapokharel <pokharel.apurba@gmail.com>
2024-03-21 20:56:29 -0500

    Initial commit

---
+++ dodft.js
@@ -0,0 +1,40 @@
+class Complex{
+  constructor(a,b){
+    this.re = a;
+    this.ig = b;
+  }
+  
+  add(c){
+    let r = c.re + this.re;
+    let i = c.ig + this.ig;
+    return new Complex(r, i);
+  }
+  
+  mul(c){
+    let r = c.re * this.re - c.ig * this.ig;
+    let i = c.re * this.ig + c.ig * this.re;
+    return new Complex(r, i);
+  }
+}
+function dft(x){
+  X = [];
+  let N = x.length;
+  for(let k=0; k<N; k++){
+    let sum = new Complex(0,0);
+    for(let n=0; n<N; n++){
+      let angle = (2*PI*k*n)/N;
+      c1 = new Complex(cos(angle), -sin(angle));
+      sum.add(x[n].mul(c1));
+    }
+    //need to avarage it so that it fits the frame
+    sum.re = sum.re/N;
+    sum.ig = sum.ig/N;
+    
+    let freq = k;
+    let amp = sqrt(sum.re * sum.re + sum.im * sum.im);
+    let phase = atan2(sum.im, sum.re);
+    X[k] = { re: sum.re, im: sum.im, freq, amp, phase };
+  }
+  return X;
+  
+}
---
+++ sketch.js
@@ -0,0 +1,61 @@
+let angle = 90;
+let wave = []
+let circleNo = 3;
+let fourierX;
+let x = [];
+function setup() {
+  createCanvas(600, 400);
+  const skip = 8;
+  for (let i = 0; i < drawing.length; i += skip) {
+    const c = new Complex(drawing[i].x, drawing[i].y);
+    x.push(c);
+  }
+  fourierX = dft(x);
+  // fourierX.sort((a, b) => b.amp - a.amp);
+}
+
+function draw() {
+  // noLoop();
+  noFill();
+  background(0);
+  let radius = 50;
+  
+  translate(100,300); 
+  let point_x = 0;
+  let point_y = 0;
+  
+  for(i = 0; i< fourierX.length; i++){
+    let prev_x = point_x;
+    let prev_y = point_y;
+    
+    let radius = fourierX[i].amp;
+    let phase = fourierX[i].phase;
+    let freq = fourierX[i].freq;
+
+    point_x += radius * cos(freq * angle + phase);
+    point_y += radius * sin(freq * angle + phase);
+    stroke(255,100);
+    circle(prev_x, prev_y, 2 * radius);
+
+    //create the line from circle to radius
+    stroke(255)
+    line(prev_x, prev_y, point_x, point_y);
+    prev_x = point_x;
+    prev_y = point_y;
+  }
+  
+  //draw the wave
+  wave.unshift({
+    x: point_x,
+    y: point_y
+  });
+  translate(200,0);
+  line(point_x-200, point_y, 0, wave[0])
+  beginShape();  
+  for(i = 0; i < wave.length; i++){
+    point(i, wave[i]);
+  }
+  endShape();
+  angle += 0.05;
+
+}
